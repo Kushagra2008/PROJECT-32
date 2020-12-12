19ac34b6a2ee5ccd67420e4000f90657e3b1d66b
Kushagra2008 <73027622+Kushagra2008@users.noreply.github.com>
2020-12-12 19:54:51 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,52 @@
+class Box 
+{
+  constructor(x, y, width, height)
+  {
+      var options = 
+      {
+        'restitution':0,
+        'friction':1,
+        'density':1.0
+      }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.visibility = 255
+    World.add(world, this.body);
+  }
+  display()
+  {
+    var pos = this.body.position
+    var angle = this.body.angle;
+    if (this.body.speed < 3)
+    {
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      fill("yellow")
+      rotate(angle);
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+    else
+    {
+      World.remove(world, this.body);
+      push();
+      this.visibility -= 5;
+      pop();
+      /*push();
+      fill("yellow")
+      imageMode(CENTER);
+      image(this.body.position.x, this.body.position.y, this.width, this.height)
+      pop();*/
+    }
+  }
+  score()
+  {
+    if (this.visibility < 0 && this.visibility > -100)
+    {
+      score1 += 20;
+    }
+  }
+}
+
+
